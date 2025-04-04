e2acd364f102266af67ca60a2c6561d2b704b884
ObaroJB <sobaroda91@gmail.com>
2025-04-04 10:25:33 +0100

    signin page/profile page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,66 @@
-
-export default function Home() {
+import { SlCalender } from "react-icons/sl";
+import { FaLock } from "react-icons/fa";
+import { IoPeopleSharp } from "react-icons/io5";
+import { FaMoneyBillWave } from "react-icons/fa";
+export default function Pain() {
+return (
     
-  return (
     <main>
+<div className="min-h-screen bg-blue-900 text-white">  
 
+
+
+<div className="text-center py-16 px-4">
+    <h2 className="text-4xl font-bold">Fast. Easy. Loans your way.</h2>
+<div className="mt-6">
+      <input type="text" placeholder="Phone Number" className="text-xl text-black" />
+      <button className="bg-green-500 px-4 py-2 ml-2">Get a Loan</button>
+</div>
+    <p className="text-sm mt-2">Checking for loan offers is completely free. We never share your information. </p>
+     <a href = "#">  Terms & Conditions</a>
+<ul className="mt-4 ">
+      <li> *Receive funds in 2 minutes or less</li>
+      <li> *No documentation or collateral</li>
+      <li> *Interest as low as 5%</li>
+</ul>
+</div>
+  
+<div className="bg-blue-800 py-8 text-center">
+<div className="grid grid-cols-2 md:grid-cols-4 gap-6">
+<div>
+        <p className=" ml-39"><SlCalender/></p>
+        <p className="text-2xl font-bold">24/7</p>
+        <p>Always available</p>
+</div>
+<div>
+        <p className="ml-39"><FaLock/></p>
+        <p className="text-2xl font-bold">Safe</p>
+        <p>Always secure</p>
+</div>
+<div>
+        <p className="ml-39"><IoPeopleSharp/></p>
+        <p className="text-2xl font-bold">1M+</p>
+        <p>Clients</p>
+</div>
+<div>
+        <p className="ml-39"><FaMoneyBillWave/></p>
+        <p className="text-2xl font-bold">40B+</p>
+        <p>Borrowed</p>
+</div>
+</div>
+</div>
+</div>
+
+
+
+
+
+       
+       
     </main>
   );
 }
+
+
+
+ 
---
+++ components/Footer.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { FaFacebook } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa6";
+import { FaTwitter } from "react-icons/fa";
+
+
+export default  function Footer (){
+    const year = new Date().getFullYear()
+    return (
+        
+        <footer className="grid grid-cols-1 gap-4 md: grid-cols-2 px-8 lg:grid-cols-3 py-4 px-2 lg:gap-0">
+        <div>
+          <p className="text-2xl text-gray-800 font-bold"> UrgentCa$h</p>
+          <p className="text-xs text-gray-600"> &copy; {year} Urgent limited</p>
+        </div>
+        <div>
+            <p  className="text-md text-gray-700">Head Office</p>
+            <p className="text-md text-gray-700">Ademola Adetokunbo crescent, WuseII,Abuja,Nigeria.</p>
+        </div>
+        <div>
+            <ul className="flex lg:justify-end items-center gap-4">
+              <li><Link href=""><FaFacebook className="text-lg"/></Link></li>
+              <li><Link href=""><FaTiktok className="text-lg"/></Link></li>
+              <li><Link href=""><FaTwitter className="text-md"/></Link></li>
+              </ul>
+
+              <ul className = "flex lg:justify-end items-center gap-4">
+              <li><Link href="" className="text-sm text-gray-600"></Link>Terms of use</li>
+              <li><Link href="" className="text-sm text-gray-600"></Link>Privacy Policy</li>
+              <li><Link href="" className="text-sm text-gray-600"></Link>Bug Bounty</li>
+              </ul>
+        </div>
+        </footer>
+        
+    )
+}
