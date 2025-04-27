418c1f59e5fb0acf5536f0c5d82cdd726f1b72a4
ObaroJB <sobaroda91@gmail.com>
2025-04-27 19:45:40 +0100

    vercel test

--- app/dashboard/get-loan/get-loan.js
+++ app/dashboard/get-loan/get-loan.js
@@ -57,7 +57,7 @@ export default function GetLoan ({userId}){
        .catch(e => {
         setOpsProgress(false);
         console.error(e);
-        alert(`you have encountered an error: ${e}`)
+        alert(` Something went wrong : ${e}`)
        })
     },
     validationSchema:schema
